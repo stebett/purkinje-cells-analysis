095be4e43991119559a4b017f5a7c9c9465edd60
Thinkpad <ginkobab@gmail.com>
2020-11-10 15:32:44 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a research project targeting the dynamics of purkinje neurons in cerebellum and the role of correleations between neighboring neurons. The work is being done at École normale supérieure in Paris under the supervision of Clément Léna.
+
+![psth](https://github.com/stebett/purkinje-cells-analysis/blob/master/plots/psth.png)
--- _research/speed.jl
+++ _research/speed.jl
@@ -7,26 +7,18 @@ include(srcdir("spike-tools.jl"))
 using Statistics
 
 
-speeds = []
-kept= []
+speeds = Float64[]
 
-for (i, (lifts, covers)) in enumerate(zip(data.lift, data.cover))
-    if (length(lifts) != length(covers)) || (-1. in lifts) || (-1. in covers)
+for (lift, cover) in zip([l[1] for l=data.lift], [c[1] for c=data.cover])
+    if (length(lift) != length(cover)) || (-1. in lift) || (-1. in cover)
         continue
     end
-    push!(speeds, (covers .- lifts))
-    push!(kept, i)
+    push!(speeds, (cover .- lift))
 end
 
+n = normalize(data.t, [l[1] for l=data.lift], (-500, 500), (-500, 500))
+mean_rate = [mean(skipnan(n[:, i])) for i=1:size(n,2)]
 
-speeds1 = [s[1] for s=speeds]
-nans = isnan.(speeds1)
-speeds1 = speeds1[.!nans]
+cor(n, speeds)
 
-n1 = normalize(data.t, [l[1] for l=data.lift], around=(-500, 500), over=(-5000, -3000))
-mean_fr = [mean(skipnan(x)) for x=eachcol(n1)]
-mean_fr = mean_fr[kept]
-
-
-
-cor(speeds1, mean_fr)
+# Decoding neuronal spikes
