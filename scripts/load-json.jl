b4d8bd53e012420a72bfa5e7a90ad88a1526d325
Stefano BETTANI <bettani@nrpc149.ens.fr>
2021-02-15 14:53:10 +0100

    about to build df with full data

--- scripts/load-json.jl
+++ scripts/load-json.jl
@@ -2,12 +2,37 @@ using DrWatson
 @quickactivate "ens"
 
 using DataFrames
+using JSON3
 
 json_string = read(datadir("full_data.json"));
 json_obj = JSON3.read(json_string);
 
-data_dict = Dict(json_obj);
+
+function search_routes(x, l, saved_list::Vector)
+	i = 0
+	for k in keys(x)
+		if x[k] isa JSON3.Object
+			idx = i+l
+			search_routes(x[k], l+1, [saved_list..., "l$idx" => k])
+			i += 1
+		else
+			global megalist
+			push!(megalist, [saved_list..., k => x[k]])
+		end
+	end
+end
+
+global megalist = []
+v = []
+search_routes(json_obj, 1, v)
+
+dict_list = []
+for i = 1:32:length(megalist)-31
+	push!(dict_list, union(megalist[i:i+31]...))
+end
 
 
-df = DataFrame(rat=String[], site=String[], tetrode=String[], neuron=String[], lift=Float64[], cover=Float64[], grasp=Float64[], t=Array{Number, 1}[])
+function nodouble(x)
+
+
 
