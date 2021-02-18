1ff74ab83e030529ff5843f902ecc467875c81ac
Stefano Bettani <bettani@biologie.ens.fr>
2021-02-18 18:56:58 +0100

    Figure 3A

--- scripts/cross-correlogram.jl
+++ scripts/cross-correlogram.jl
@@ -36,11 +36,35 @@ active_neigh = get_pairs(active, "n")
 idx1, idx2 = active_neigh[2]
 idx1, idx2 = 437, 438
 
+tmp = data;
+
 x = section(tmp[findall(tmp.index .== idx1), "t"], tmp[findall(tmp.index .== idx1), "cover"], [-400, 400], binsize=.5) 
+c1 = sort_active(hcat(x...), 10)
+heatmap(c1', c=:grays, cbar=false)
+xticks!([0, 800, 1595], ["-400", "0", "400"])
+title!("Spiketrain 437")
+xlabel!("Time (ms)")
+p1 = ylabel!("Trials")
+x_fr = section(tmp[findall(tmp.index .== idx1), "t"], tmp[findall(tmp.index .== idx1), "cover"], [-400, 400], binsize=.5, :conv, :avg) 
+plot(x_fr, legend=false)
+ylabel!("Firing rate")
+p2= xticks!([0, 800, 1595], ["-400", "0", "400"])
+
+
+
 y = section(tmp[findall(tmp.index .== idx2), "t"], tmp[findall(tmp.index .== idx2), "cover"], [-400, 400], binsize=.5) 
+c2 = sort_active(hcat(y...), 10)
+heatmap(c2', c=:grays, cbar=false)
+xticks!([0, 800, 1595], ["-400", "0", "400"])
+title!("Spiketrain 438")
+p3 = xlabel!("Time (ms)")
+y_fr = section(tmp[findall(tmp.index .== idx2), "t"], tmp[findall(tmp.index .== idx2), "cover"], [-400, 400], binsize=.5, :conv, :avg) 
+plot(y_fr, legend=false)
+p4= xticks!([0, 800, 1595], ["-400", "0", "400"])
+plot(p1, p3, p2, p4, layout = @layout [ a b ; c d ])
 
 heatmap(hcat(crosscor_custom.(x, y)...)')
-#savefig(plotsdir("crosscor", "heatmap"), "scripts/cross-correlogram.jl")
+#savefig(plotsdir("crosscor", "heatmap-couple"), "scripts/cross-correlogram.jl")
 
 #>
 #< Heatmap all couples
