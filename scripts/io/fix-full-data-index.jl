1255beea2260324709030d49f94a87c0385584c4
Stefano Bettani <bettani@biologie.ens.fr>
2021-02-19 11:56:30 +0100

    fixed data_full index

--- scripts/io/fix-full-data-index.jl
+++ scripts/io/fix-full-data-index.jl
@@ -3,4 +3,13 @@ using DrWatson
 
 include(scriptsdir("io", "load-full.jl"))
 
+function extract_index(df::DataFrame, path::String, name::String)::Int
+	rat, block = split(path, "/")[[5, 7]]
+	site = block[6:end]
+	tet, cell = split(name, ".")
+	neuron = replace(cell, "t"=>"neuron")
+	idx = df[((df.rat .== rat) .& (df.site .== site) .& (df.tetrode .== tet) .& (df.neuron .== neuron)), "index"]
 
+	@assert length(idx) == 1
+	idx[1]
+end
--- scripts/io/load-full.jl
+++ scripts/io/load-full.jl
@@ -78,6 +78,18 @@ function load_full()
 	df
 end
 
+function extract_index(df::DataFrame, path::String, name::String)::Int
+	rat, block = split(path, "/")[[5, 7]]
+	site = block[6:end]
+	tet, cell = split(name, ".")
+	neuron = replace(cell, "t"=>"neuron")
+	idx = df[((df.rat .== rat) .& (df.site .== site) .& (df.tetrode .== tet) .& (df.neuron .== neuron)), "index"]
+
+	@assert length(idx) == 1
+	idx[1]
+end
+
 data_full = load_full()
+data_full["index"] = extract_index.(data, data_full.path, data_full.name)
 
 export data_full
