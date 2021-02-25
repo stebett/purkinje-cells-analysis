3b6392dbec34b6616487142caaf8c0300438766c
Stefano Bettani <bettani@biologie.ens.fr>
2021-02-25 11:32:19 +0100

    figure 3c complete

--- scripts/cross-correlogram.jl
+++ scripts/cross-correlogram.jl
@@ -18,9 +18,9 @@ function sem(x::Matrix; dims=2)
 end
 
 #<
-tmp = data[data.p_acorr .< 0.5, :];
-neigh = get_pairs(tmp, "n")
-dist = get_pairs(tmp, "d")
+tmp = data[data.p_acorr .< 0.2, :];
+neigh = get_pairs(tmp, "n");
+dist = get_pairs(tmp, "d");
 
 
 #>
@@ -96,20 +96,42 @@ savefig(plotsdir("crosscor", "figure_3b"), "scripts/cross-correlogram.jl")
 #>
 #< 3C
 
+merge(ranges, x) = Tuple{Float64, Float64}[ranges[x[1]]..., ranges[x[2]]...]
 
-n, r = sectionTrial(tmp, 6, 1000., 200)
-indexes = get_active_ranges(tmp, thr=1.5) 
-active(x) = Tuple{Float64, Float64}[indexes[x[1]]..., indexes[x[2]]...]
-ranges = active.(neigh)
-heatmap(hcat(vcat.(section.(Ref(tmp[tmp.index .== neigh[1][1], :t]), Ref(tmp[tmp.index .== neigh[1][1], :cover]), ranges[1])...)...))
+tmp = data;
+pad = 1000.
+num_bins = 6
+b = 200
+binsize=.5
+n, r = sectionTrial(tmp, num_bins, pad, b, :mad);
+todrop = drop(n, index=true) .* tmp.index
+todrop = todrop[todrop .> 0]
+ranges = get_active_ranges(tmp, num_bins=num_bins, pad=pad, b=b)
+neigh = get_pairs(tmp, "n")
+merged_ranges = merge.(Ref(ranges), neigh)
 
+c = []
+for (cell, bad, rng) = zip(neigh, todrop, merged_ranges)
+	if cell[1] ∉ todrop && cell[2] ∉ todrop && !isempty(rng)
+		c1 = vcat.(section.(Ref(tmp[tmp.index .== cell[1], :t]), Ref(tmp[tmp.index .== cell[1], :cover]), rng, binsize=binsize)...)
+		c2 = vcat.(section.(Ref(tmp[tmp.index .== cell[2], :t]), Ref(tmp[tmp.index .== cell[2], :cover]), rng, binsize=binsize)...)
+		c3 = crosscor.(c1, c2, true, binsize=binsize) |> x->hcat(x...) |> drop |> x->mean(x, dims=2)
 
-neighbors = crosscor(tmp, neigh, ranges, binsize=0.5, :norm) |> drop
+		fr1 = hcat(c1...) |> drop |> mean
+		fr2 = hcat(c2...) |> drop |> mean
+
+		if fr1 >= 0.01 && fr2 >= 0.01
+			push!(c, c3)
+		end
+	end
+end
+
+neighbors = hcat(c...) |> drop
 
 mean_neighbors = mean(neighbors, dims=2)[:]
 sem_neighbors = sem(neighbors, dims=2)[:]
 
-mean_neighbors[40:42] .= NaN 
+mean_neighbors[41:42] .= NaN 
 
 plot(mean_neighbors, c=:red, ribbon=sem_neighbors, fillalpha=0.3,  linewidth=3, label=false)
 xticks!([1:10:81;],["$i" for i =-20:5:20])
--- test/cross-corr-plot-test.jl
+++ test/cross-corr-plot-test.jl
@@ -8,54 +8,43 @@ include(srcdir("section-trial.jl"))
 include(srcdir("plot", "psth.jl"))
 include(srcdir("plot", "cross-correlation.jl"))
 
-tmp = data[data.p_acorr .< 0.5, :];
 
-function test_active(df, idx, rng)
-	c1 = vcat.(section.(Ref(df[df.index .== idx[1], :t]), Ref(df[df.index .== idx[1], :cover]), rng)...)
-	c2 = vcat.(section.(Ref(df[df.index .== idx[2], :t]), Ref(df[df.index .== idx[2], :cover]), rng)...)
-	c3 = crosscor.(c1, c2, false, binsize=1.) |> x->hcat(x...) |> drop |> x->mean(x, dims=2)
-
-	fr1 = hcat(c1...) |> drop |> mean |> x->round(x, digits=3)
-	fr2 = hcat(c2...) |> drop |> mean |> x->round(x, digits=3)
-	if fr1 < 0.01 || fr2 < 0.01
-		return
-	end
-
-	heatmap(hcat(c1...)')
-	p1 = title!("Mean Firing rate: $fr1")
-	heatmap(hcat(c2...)')
-	p2 = title!("Mean Firing rate: $fr2")
-	p3 = plot(c3)
-	
-	l = @layout [ a b ; c ]
-	p = plot(p1, p2, p3, layout=l)
-
-	savefig(p, plotsdir("crosscor", "test", "$idx"))
-	return c3
-end
-
-
-function test_all_active(df)
+function test_all_active(df, saveplot=false)
 	pad = 1000.
 	num_bins = 6
 	b = 200
+	binsize=.5
 	n, r = sectionTrial(df, num_bins, pad, b, :mad);
-	todrop = drop(n, index=true)
+	todrop = drop(n, index=true) .* df.index
+	todrop = todrop[todrop .> 0]
 	ranges = get_active_ranges(df, num_bins=num_bins, pad=pad, b=b)
-	active(ranges, x) = Tuple{Float64, Float64}[ranges[x[1]]..., ranges[x[2]]...]
-	neigh = get_pairs(tmp, "n")
-	active_bins = active.(Ref(ranges), neigh)
+	neigh = get_pairs(df, "n")
+	merged_ranges = merge.(Ref(ranges), neigh)
 
 	c = []
-	for (idx, bad, act) = zip(neigh, todrop, active_bins)
-		if bad
-			nothing
-		elseif isempty(act)
-			nothing
-		else
-			push!(c, test_active(df, idx, act))
+	for (cell, bad, rng) = zip(neigh, todrop, merged_ranges)
+		if cell[1] ∉ todrop && cell[2] ∉ todrop && !isempty(rng)
+			c1 = vcat.(section.(Ref(df[df.index .== cell[1], :t]), Ref(df[df.index .== cell[1], :cover]), rng, binsize=binsize)...)
+			c2 = vcat.(section.(Ref(df[df.index .== cell[2], :t]), Ref(df[df.index .== cell[2], :cover]), rng, binsize=binsize)...)
+			c3 = crosscor.(c1, c2, true, binsize=binsize) |> x->hcat(x...) |> drop |> x->mean(x, dims=2)
+
+			fr1 = hcat(c1...) |> drop |> mean
+			fr2 = hcat(c2...) |> drop |> mean
+
+			if fr1 >= 0.01 && fr2 >= 0.01
+				if saveplot
+					heatmap(hcat(c1...)')
+					p1 = title!("Mean Firing rate: $fr1")
+					heatmap(hcat(c2...)')
+					p2 = title!("Mean Firing rate: $fr2")
+					p3 = plot(c3)
+					
+					l = @layout [ a b ; c ]
+					p = plot(p1, p2, p3, layout=l)
+
+					savefig(p, plotsdir("crosscor", "test", "$cell"))
+				end
+			end
 		end
 	end
 end
-
-test_all_active(tmp)
