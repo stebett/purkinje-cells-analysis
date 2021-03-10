0013b7beeb827c20f96a850cfd48328134c5b9eb
Thinkpad <ginkobab@gmail.com>
2021-03-10 13:41:35 +0100

    added rangeT

--- scripts/spline.jl
+++ scripts/spline.jl
@@ -1,8 +1,53 @@
 using DrWatson
 @quickactivate :ens
 
-using JLD 
+using JLD2 
+using Spikes
+using RCall
 
-results = load(datadir("spline", "gssmodels.jld"), "iter1")
+include(srcdir("spline-plots.jl"))
+
+results = load("data/spline/gssmodels.jld2", "results")
+
+s_isi = results[556][1]
+
+function above(x::Dict)
+end
+
+x = s_isi
+
+y = x[:est_mean] .- x[:est_sd] .> 0
 
 
+""" 
+Takes first and last value of `x` of the `true` indices of `y`
+"""
+function rangeT(x::AbstractVector, y::BitArray{1})
+	ranges = []
+	find_start = true
+	start = NaN
+	finish = NaN
+	for (i, v) in enumerate(y)
+		if find_start
+			if v 
+				start = x[i]
+				find_start = false
+			end
+		else
+			if !v
+				finish = x[i-1]
+				push!(ranges, (start, finish))
+				find_start = true
+			end
+		end
+	end
+	ranges
+end
+		
+
+
+
+
+
+
+plot_quick_prediction(s_isi)
