ef25241005f61310aa0fac3d5edc4524cba23e77
Thinkpad <ginkobab@gmail.com>
2021-04-30 13:37:35 +0200

    parametrized number of sims

--- scripts/spline/modular/simulate.R
+++ scripts/spline/modular/simulate.R
@@ -1,6 +1,7 @@
 library(STAR)
+library(configr)
 
-sim.all <- function(file, n=5) {
+sim.all <- function(file, n) {
 	res = read_rdata(file)
 	lfun = lapply(res$rnfun, mkSelf)[1] 
 	trials = lapply(split(res$data, res$data$trial), function(x) x[, names(res$gss$mf)])
@@ -23,12 +24,16 @@ read_rdata <- function(file) {
 # inpath = "data/analyses/spline/batch-artificial/best-neigh/out/data/" 
 
 args <- commandArgs(trailingOnly = TRUE)
-inpath = paste(args[1], "out", "data", sep="/")
+configpath = configfile
+configpath = paste(args[1], "config.toml", sep="/")
+inpath = paste(args[2], "out", "data", sep="/")
+outpath = args[3]
 
+config = read.config(file = configpath)
+n_sims = config$mkdf$n_sims
 infiles <- list.files(path=inpath, pattern=".R", full.names=T, all.files=T)
-simulations = lapply(infiles, sim.all)
+simulations = mapply(sim.all, infiles, n_sims)
 
 
 # outpath = "data/analyses/spline/batch-8/best-neigh/post-proc/simulated.rds"
-outpath = args[2]
 saveRDS(simulations, outpath)
